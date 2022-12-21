96c22af088e0d43f92d486d8065b37815d475695
Aleksandra Bukalskai <bukalska.aleksandra@gmail.com>
2022-12-21 11:40:36 +0000

    Add media queries for small devices

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './Card.module.scss';
 import Header from '../Header/Header';
 import Display from '../Display/Display';
@@ -16,6 +16,7 @@ const Card = () => {
         peopleAmount: Number(peopleAmount) >= 0 ? Number(peopleAmount) : null,
         bill: Number(bill) >= 0 ? Number(bill) : null,
         tip: Number(tip) || 0,
+        customTip: Number(customTip) || 0
     }
 
     const handleReset = () => {
@@ -25,12 +26,16 @@ const Card = () => {
     }
 
     const resetDisabled =
-        bill === '' || bill === 0 && tip === '' && customTip === '' && peopleAmount === '' || peopleAmount === 0 ? true : null;
+        (bill === '' || bill === 0) && tip === '' && customTip === '' && (peopleAmount === '' || peopleAmount === 0) ? true : null;
+
+    useEffect(() => {
+        data.customTip > 0 ? setActiveButton(null) : setActiveButton(true)
+    }, [data.customTip])
 
 
 
     const tipPerPerson = data.peopleAmount
-        ? data.bill * (customTip ? Number(customTip) : data.tip) / 100 / data.peopleAmount
+        ? data.bill * (data.customTip > 0 ? data.customTip : data.customTip < 0 ? 0 : data.tip ) / 100 / data.peopleAmount
         : 0
 
     const totalPerPerson = data.peopleAmount ? data.bill / data.peopleAmount + tipPerPerson : 0;
